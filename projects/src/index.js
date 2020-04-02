d3bd30563539f2b7ced6b7d362d84a8b433d80ce
shivgaurav7727 <shivgaurav27@gmail.comm>
2020-04-03 01:23:57 +0530

    bootstrap setup

--- projects/src/index.js
+++ projects/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
+
 
 ReactDOM.render(
   <React.StrictMode>
